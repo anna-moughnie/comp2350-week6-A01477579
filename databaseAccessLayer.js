d6c72d1e988038e8f86e15a6b23c037ee6b48637
Anna Moughnie <anna@moughnie.com>
2026-02-17 22:25:15 -0800

    please work

--- databaseAccessLayer.js
+++ databaseAccessLayer.js
@@ -1,48 +1,46 @@
-const database = include('/databaseConnection');
-const bcrypt = require('bcrypt');
+const database = include("/databaseConnection");
+const bcrypt = require("bcrypt");
 const saltRounds = 12;
 
 async function getAllUsers() {
-	let sqlQuery = `
+  let sqlQuery = `
 		SELECT * FROM web_user;
 	`;
 
-	try {
-		const results = await database.query(sqlQuery);
-		console.log(results[0]);
-		return results[0];
-	}
-	catch (err) {
-		console.log("Error selecting from user table");
-		console.log(err);
-		return null;
-	}
+  try {
+    const results = await database.query(sqlQuery);
+    console.log(results[0]);
+    return results[0];
+  } catch (err) {
+    console.log("Error selecting from user table");
+    console.log(err);
+    return null;
+  }
 }
 
 async function addUser(postData) {
-	let hashedPassword = await bcrypt.hash(postData.password, saltRounds);
-	let sqlInsertUser = `
-INSERT INTO web_user (first_name, last_name, email, password_hash)
+  let hashedPassword = await bcrypt.hash(postData.password, saltRounds);
+  let sqlInsertUser = `
+INSERT INTO web_user (?, ?, ?, ?)
 VALUES (:first_name, :last_name, :email, :hashedPasword);
 `;
-	let params = {
-		first_name: postData.first_name,
-		last_name: postData.last_name,
-		email: postData.email,
-		hashedPassword: hashedPassword
-	};
-	console.log(sqlInsertUser);
-	try {
-		const results = await database.query(sqlInsertUser, params);
-		let insertedID = results[0].insertId;
-		console.log("🙋Inserted new user with ID:");
-		console.log(insertedID);
-		return true;
-	}
-	catch (err) {
-		console.log(err);
-		return false;
-	}
+  let params = {
+    first_name: postData.first_name,
+    last_name: postData.last_name,
+    email: postData.email,
+    hashedPassword: hashedPassword,
+  };
+  console.log(sqlInsertUser);
+  try {
+    const results = await database.query(sqlInsertUser, params);
+    let insertedID = results[0].insertId;
+    console.log("🙋Inserted new user with ID:");
+    console.log(insertedID);
+    return true;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
 }
 
-module.exports = { getAllUsers, addUser }
+module.exports = { getAllUsers, addUser };
