17d556d642430abf27f258d8853dc9a4ef62319f
Anna Moughnie <anna@moughnie.com>
2026-02-17 23:20:06 -0800

    does this work?

--- databaseAccessLayer.js
+++ databaseAccessLayer.js
@@ -43,4 +43,22 @@ VALUES (:first_name, :last_name, :email, :hashedPassword);
   }
 }
 
-module.exports = { getAllUsers, addUser };
+async function deleteUser(webUserId) {
+  let sqlDeleteUser = `
+DELETE FROM web_user
+WHERE web_user_id = :userID
+`;
+  let params = {
+    userID: webUserId,
+  };
+  console.log(sqlDeleteUser);
+  try {
+    await database.query(sqlDeleteUser, params);
+    return true;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+}
+
+module.exports = { getAllUsers, addUser, deleteUser };
